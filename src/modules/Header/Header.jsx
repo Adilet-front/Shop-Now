faf7ff2663a6f413fa9cd9eb8af8502ddff4100c
Adilet333 <adilettinadilettin@gmail.com>
2025-06-20 15:27:48 +0600

    header

--- src/modules/Header/Header.jsx
+++ src/modules/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router";
+import { NavLink } from "react-router-dom";
 import styles from "./Header.module.scss";
 import { useTranslation } from "react-i18next";
 
@@ -16,48 +16,41 @@ export const Header = () => {
           <div className={styles.text}>
             <p className={styles.text1}>
               {t("start_block.text1")}
-              <NavLink to={""}>ShopNow</NavLink>
+              <NavLink to={""}>{t("start_block.BuyNow")}</NavLink>
             </p>
           </div>
 
           <div className={styles.languageSelector}>
-            <button
-              onClick={() => changeLanguage("en")}
+            <select
+              onChange={(e) => changeLanguage(e.target.value)}
               className={styles.enSvg}
             >
-              {t("start_block.button_En")}
-              <div className={styles.iconLang}>
-                <img src="/images/Ensvg.svg" alt="EN" />
-              </div>
-            </button>
-
-            <div className={styles.langDropDown}>
-              <button onClick={() => changeLanguage("ru")}>
-                {t("start_block.button_Ru")}
-              </button>
-            </div>
+              <option value="en">English</option>
+              <option value="ru">Русский</option>
+            </select>
           </div>
         </div>
       </div>
       <div className={styles.downHeader}>
-        <h2>Exclusive</h2>
+        <h2>{t("start_block.Exclusive")}</h2>
         <nav>
           <li>
-            <NavLink>Home</NavLink>
+            <NavLink>{t("start_block.main")}</NavLink>
           </li>
           <li>
-            <NavLink>Contact</NavLink>
+            <NavLink>{t("start_block.Contact")}</NavLink>
           </li>
           <li>
-            <NavLink>About</NavLink>
+            <NavLink>{t("start_block.About")}</NavLink>
           </li>
           <li>
-            <NavLink>Sign Up</NavLink>
+            <NavLink>{t("start_block.SignUp")}</NavLink>
           </li>
         </nav>
         <div className={styles.inputWrapper}>
           <div className={styles.InInputWrapper}>
-            <input type="text" placeholder="What are you looking for?" />
+            <input type="text" placeholder={t("start_block.lookFor")} />
+
             <NavLink>
               <img
                 className={styles.searchSvg}
