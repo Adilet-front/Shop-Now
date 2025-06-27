d5d4f25f92380b6d11837502cf2aba6117be8854
Adilet333 <adilettinadilettin@gmail.com>
2025-06-28 00:39:11 +0600

    fetch

--- src/modules/ProductCard/ProductCard.jsx
+++ src/modules/ProductCard/ProductCard.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
 
-export const ProductCard = ()=> {
-return(
-  <div>
-    
-  </div>
-)
-}
+export const ProductCard = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/products?_limit=90")
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error("Ошибка при получении данных:", error));
+  }, []);
+
+  return (
+    <div>
+      <h2>Список продуктов</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} – ${product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
