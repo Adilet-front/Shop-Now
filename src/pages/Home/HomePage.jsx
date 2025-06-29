ad66470b81addba89cd42a9327cd7145de104251
h1da90 <lozailuna08@gmail.com>
2025-06-29 20:02:18 +0600

    ilya

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -2,12 +2,14 @@ import { Bonjour } from "../../modules/Bonjour/Bonjour";
 import { NewArrival } from "../../modules/NewArrival/NewArrival";
 import { HeaderPart2 } from "../../modules/HeaderPart2/HeaderPart2";
 import { ProductCard } from "../../modules/ProductCard/ProductCard";
+import { ThisMonth } from "../../modules/iBlock1/ThisMonth";
 
 export const HomePage = () => {
   return (
     <div className="HomePage">
       <HeaderPart2 />
       <ProductCard />
+      <ThisMonth />
       <NewArrival />
       <Bonjour />
     </div>
