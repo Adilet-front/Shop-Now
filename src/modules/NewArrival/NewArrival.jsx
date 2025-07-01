14c23da3fa19f99ca052e1bc65a461d08249fa45
alt1233 <akylbekovaaltynaj7@gmail.com>
2025-07-01 17:35:00 +0600

    new_arrival_end

--- src/modules/NewArrival/NewArrival.jsx
+++ src/modules/NewArrival/NewArrival.jsx
@@ -12,66 +12,62 @@ export const NewArrival = () => {
           <div className={styles.NewArrivalBox}></div>
           <h2>Featured</h2>
         </div>
-        <h2>{t("new_arrival.New_Arrival")}</h2>
+        <div className={styles.NewArrivalH1}>
+          <h1>{t("new_arrival.New_Arrival")}</h1>
+        </div>
+      </div>
+
+      <div className={styles.NewArrivalGen}>
+        <div className={styles.NewArrivalLeft}>
+          <div className={styles.NewArrivalLeftImg}>
+            <img src="/public/images/playstation.png" alt="фотка плойки" />
+          </div>
+          <div className={styles.NewArrivalLeftText}>
+            <h1>PlayStation 5</h1>
+            <p>{t("new_arrival.New_Arrival_text")}</p>
+            <NavLink>{t("new_arrival.shop_now")}</NavLink>
+          </div>
         </div>
 
-        <div className={styles.NewArrivalGen}>
-          
-          <div className={styles.NewArrivalLeft}>
-            <div className={styles.NewArrivalLeftImg}>
-              <img src="/public/images/playstation.png" alt="фотка плойки" />
+        <div className={styles.NewArrivalRight}>
+          <div className={styles.NewArrivalRightTop}>
+            <div className={styles.NewArrivalRightTopImg}>
+              <img src="/public/images/women.png" alt="фото жeнщины" />
             </div>
-            <div className={styles.NewArrivalLeftText}>
-              <h1>PlayStation 5</h1>
-              <p>{t("new_arrival.New_Arrival_text")}</p>
+            <div className={styles.NewArrivalRightToptext}>
+              <h1>{t("new_arrival.womens_collection")}</h1>
+              <p>{t("new_arrival.womens_collection_text")}</p>
               <NavLink>{t("new_arrival.shop_now")}</NavLink>
             </div>
           </div>
 
-          <div className={styles.NewArrivalRight}>
-            <div className={styles.NewArrivalRightTop}>
-              <div className={styles.NewArrivalRightTopImg}>
-                <img src="/public/images/women.png" alt="фото жeнщины" />
+          <div className={styles.NewArrivalRightUnder}>
+            <div className={styles.NewArrivalRightOne}>
+              <div className={styles.NewArrivalRightOneImg}>
+                <img src="/public/images/speakers.png" alt="колонка" />
               </div>
-              <div className={styles.NewArrivalRightToptext}>
-                <h1>{t("new_arrival.womens_collection")}</h1>
-                <p>{t("new_arrival.womens_collection_text")}</p>
+              <div className={styles.NewArrivalRightOneText}>
+                <h1>{t("new_arrival.speakers")}</h1>
+                <p>{t("new_arrival.speakers_text")}</p>
                 <NavLink>{t("new_arrival.shop_now")}</NavLink>
               </div>
             </div>
 
-            <div className={styles.NewArrivalRightUnder}>
-              <div className={styles.NewArrivalRightOne}>
-                <div className={styles.NewArrivalRightOneImg}>
-                  <img src="/public/images/speakers.png" alt="колонка" />
-                </div>
-                <div className={styles.NewArrivalRightOneText}>
-                  <h1>{t("new_arrival.speakers")}</h1>
-                  <p>{t("new_arrival.speakers_text")}</p>
-                  <NavLink>{t("new_arrival.shop_now")}</NavLink>
-                </div>
+            <div className={styles.NewArrivalRightTwo}>
+              <div className={styles.NewArrivalRightTwoImg}>
+                <img src="/public/images/parfume.png" alt="фото духов" />
               </div>
-
-              <div className={styles.NewArrivalRightTwo}>
-                <div className={styles.NewArrivalRightTwoImg}>
-                  <img src="/public/images/parfume.png" alt="фото духов" />
-                </div>
-                <div className={styles.NewArrivalRightTwoText}>
-                  <h1>{t("new_arrival.Perfume")}</h1>
-                  <p>{t("new_arrival.Perfume_text")}</p>
-                  <NavLink>{t("new_arrival.shop_now")}</NavLink>
-                </div>
+              <div className={styles.NewArrivalRightTwoText}>
+                <h1>{t("new_arrival.Perfume")}</h1>
+                <p>{t("new_arrival.Perfume_text")}</p>
+                <NavLink>{t("new_arrival.shop_now")}</NavLink>
               </div>
             </div>
           </div>
         </div>
+      </div>
     </>
   );
 };
 
-
-
-
-
-
-// 
+//
