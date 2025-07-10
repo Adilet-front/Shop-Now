0faa4d25108ee4702c3bd699545c3fed8c67c5dc
Adilet333 <adilettinadilettin@gmail.com>
2025-07-10 22:29:20 +0600

    changes

--- src/pages/OurStory/Part2OurStory/Part2OurStory.jsx
+++ src/pages/OurStory/Part2OurStory/Part2OurStory.jsx
@@ -3,6 +3,9 @@ import { NavLink } from "react-router-dom";
 import styles from "./Part2OurStory.module.scss";
 export const Part2OurStory = () => {
   const { t } = useTranslation();
+
+
+  
   return (
     <>
       <div className={styles.Part2OurStoryGen}>
@@ -16,18 +19,16 @@ export const Part2OurStory = () => {
           <h1>33k</h1>
           <p>{t("About_Us.about_us_text6")}</p>
         </div>
-<div className={styles.Part2OurStoryPart3}>
+        <div className={styles.Part2OurStoryPart3}>
           <img src="/public/images/AboutUs3.png" alt="картинка3" />
           <h1>45.5k</h1>
           <p>{t("About_Us.about_us_text7")}</p>
         </div>
-<div className={styles.Part2OurStoryPart4}>
+        <div className={styles.Part2OurStoryPart4}>
           <img src="/public/images/aboutUs4.png" alt="картинка4" />
           <h1>25k</h1>
           <p>{t("About_Us.about_us_text8")}</p>
         </div>
-
-
       </div>
     </>
   );
